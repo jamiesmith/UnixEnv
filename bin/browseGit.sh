f808e4aff2e2009b861b1aa5fdb954d02b2daafe
Jamie Smith <github@jamieandamy.com>
2018-07-24 13:23:52 -0400

    minor tweaks

--- bin/browseGit.sh
+++ bin/browseGit.sh
@@ -4,12 +4,13 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 
 repo=$(git config --get remote.origin.url | sed 's|git@github.com:||; s|https://github.com/||; s|.git$||')
 
-if [ "$branch" = "master" ]
-then
-    url="https://github.com/${repo}.git"
-else
-    url="https://github.com/${repo}/tree/${branch}"
-fi
+root=$(git rev-parse --show-toplevel)
+relativeDir=${PWD/#$root}
+
+echo root is $root
+
+
+url="https://github.com/${repo}/tree/${branch}$relativeDir"
 
 echo opening $url
 
