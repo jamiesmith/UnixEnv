f905584a3ce7c3c13e44fdea66aa55f37a2249b0
Jamie Smith <jamie@jamieandamy.com>
2024-08-08 15:58:21 -0400

    minor changes

--- bin/browseGit.sh
+++ bin/browseGit.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
 
-branch=$(git rev-parse --abbrev-ref HEAD)
+quiet=""
 
+while getopts "q" option
+do
+    case $option in 
+	q)
+	    quiet="yep"
+	    ;;
+    esac
+done
+shift $((${OPTIND} - 1)) 
+
+branch=$(git rev-parse --abbrev-ref HEAD)
 repo=$(git config --get remote.origin.url | sed 's|git@github.com:||; s|https://github.com/||; s|.git$||')
+repo=$(git config --get remote.origin.url | sed 's|git@github.com:|https://github.com/|;  s|.git$||')
 
 root=$(git rev-parse --show-toplevel)
-relativeDir=${PWD/#$root}
 
-echo root is $root
+# Hack in case we're in a symlinked dir
+#
+pwd=$(/bin/pwd -P)
+relativeDir=${pwd/#$root}
 
-url="https://github.com/${repo}/tree/${branch}$relativeDir"
-url=${url%*/tree/master}
+if [ -n "$VERBOSE" ]
+then
+    echo "branch:      [$branch]"     
+    echo "repo:        [$repo]"   
+    echo "root:        [$root]"   
+    echo "relativeDir: [$relativeDir]"          
+fi
 
-echo opening $url
-open $url
+branchPath=""
+
+url="https://github.com/${repo}"
+if [ -n "$relativeDir" ]
+then
+    branchPath="/tree/${branch}${relativeDir}"
+fi
+
+# If the branch is detached then this will just fail
+#
+url="${repo}${branchPath}"
+
+printf "Repo:\n\n%s\n\n" $url
+[ -z "$quiet" ] && open $url
